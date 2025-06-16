696c8821a98292e2516f7275c7360bab80a54398
Roman Rihter <teledurak@gmail.com>
2025-06-16 18:05:40 +0300

    apex: change message IDs

--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/notifications/Notification.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/notifications/Notification.kt
@@ -143,11 +143,12 @@ open class Notification {
         const val AAPS_DIR_NOT_SELECTED = 92
         const val EQUIL_ALARM = 93
         const val EQUIL_ALARM_INSULIN = 94
+        const val PUMP_IS_LOCKED = 95
+        const val APEX_SERVICE_MSG = 96
 
         const val USER_MESSAGE = 1000
 
         const val PERMISSION_EXTERNAL_STORAGE = 2000
-        const val APEX_SERVICE_MSG = 2001
 
         const val IMPORTANCE_HIGH = 2
         const val CATEGORY_ALARM = "alarm"
